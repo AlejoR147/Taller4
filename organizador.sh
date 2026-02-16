e77a418248da58db2641fd77320651fad21da2ba
AlejoR147 <ronan.roman10@gmail.com>
2026-02-16 14:18:20 -0500

    modificacion del script

--- organizador.sh
+++ organizador.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 read -p "¿Cómo quieres llamar a la carpeta de seguridad?: " NOMBRE_CARPETA
 mkdir "$NOMBRE_CARPETA"
-mv "datos1.txt datos2.txt notas.txt ./$NOMBRE_CARPETA/"
-chmod "700 $NOMBRE_CARPETA/"
-chmod "400 ./$NOMBRE_CARPETA/*"
+mv datos1.txt datos2.txt notas.txt $NOMBRE_CARPETA/
+chmod 700 $NOMBRE_CARPETA/
+chmod 400 $NOMBRE_CARPETA/*
 
 echo "Blindaje completado para el usuario: $USER"
 
